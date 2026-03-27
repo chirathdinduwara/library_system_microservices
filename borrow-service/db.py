c662b9ba40e0b6cf9a6dafe0e42e9e80782ab6d0
Lakshan03X <nipunlakshan9255@gmail.com>
2026-03-28 00:23:03 +0530

    creating db connection

--- borrow-service/db.py
+++ borrow-service/db.py
@@ -1,1 +1,12 @@
-hello
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME", "library_db")
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+borrow_collection = db["borrows"]
